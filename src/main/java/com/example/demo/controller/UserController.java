4ee6ed2d1f0de50e9474201ae8f9d35cf982cfda
BaeInHoo <qodlsgh0210@naver.com>
2023-11-06 19:26:48 +0900

    password security

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -24,7 +24,6 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @Autowired
     BCryptPasswordEncoder passwordEncoder;
 
     @GetMapping("/{id}")
@@ -55,18 +54,15 @@ public class UserController {
     }
 
     @RequestMapping(value = "/secu", method = RequestMethod.GET)
-    public void secu() {
+    public void secu(String pwd) {
 
-        String rawPassword = "vam123";                //인코딩 전 메서드
-        String encdoePassword1;                        // 똑같은 비밀번호 데이터를 encdoe()메서드를 사용했을 때 동일한 인코딩된 값이 나오는지 확인하기 위해 추가
+        String rawPassword = String.valueOf(userService.getPassword(pwd));                //인코딩 전 메서드
+        String encodePassword;                        // 똑같은 비밀번호 데이터를 encdoe()메서드를 사용했을 때 동일한 인코딩된 값이 나오는지 확인하기 위해 추가
 
-        encdoePassword1 = passwordEncoder.encode(rawPassword);
+        encodePassword = passwordEncoder.encode(rawPassword);
 
         // 인코딩된 패스워드 출력
-        System.out.println("encdoePassword1 : " +encdoePassword1);
-
-        String truePassowrd = "vam123";
-        String falsePassword = "asdfjlasf";
+        log.info("encodePassword : {}" ,encodePassword);
 
     }
 

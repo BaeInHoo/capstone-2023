b6ee2bdb250d1d4023dc3ae1832533246bb9acf7
BaeInHoo <qodlsgh0210@naver.com>
2023-11-11 15:41:02 +0900

    created AccidentKeywordMapping

---
+++ src/main/java/com/example/demo/domain/AccidentKeywordMapping.java
@@ -0,0 +1,21 @@
+package com.example.demo.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+
+/*
+ * created on 2023/11/11
+ * */
+@Entity
+public class AccidentKeywordMapping {
+
+    @Id
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "ACC_NO")
+    private AccidentResult accResult;
+
+    @Id
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "KEY_NO")
+    private Keyword keyword;
+}
